2b60184d423836d110036fbbfefadf9b5ce7687c
kenn <scendy17@gmail.com>
2025-03-06 15:16:29 +0700

    Update casting.py

---
+++ CASTING/casting.py
@@ -0,0 +1,14 @@
+# Casting
+# NIM : 1124102195 
+# NAMA : Scendy Aprianda Islamy
+
+#input
+a = int(input('Masukkan angka 1 : '))
+b = int(input('Masukkan angka 2 : '))
+
+#proses
+total = a + b
+
+#output
+print('Hasil penjumlahan : ', total)
+print('--------------------------------')
