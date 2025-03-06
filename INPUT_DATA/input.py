611814839f5e01c5c7a96f122f3ec95c145f2415
kenn <scendy17@gmail.com>
2025-03-06 15:01:37 +0700

    Update input.py

---
+++ INPUT_DATA/input.py
@@ -0,0 +1,10 @@
+# Input Data
+# NIM : 1124102195 
+# NAMA : Scendy Aprianda Islamy
+
+#input
+user = input('Masukan Nama : ')
+
+#output
+print('Selamat Datang', user)
+print('---------------------')
