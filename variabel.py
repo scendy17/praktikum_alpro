bea563ba2a672fabe9159f165dea71be8609f3fe
kenn <scendy17@gmail.com>
2025-03-06 14:11:54 +0700

    Add files via upload

---
+++ variabel.py
@@ -0,0 +1,9 @@
+# Variable
+# NIM : 1124102195 
+# NAMA : Scendy Aprianda Islamy
+
+angka1 = 5
+angka2 = 10
+
+total = angka1 + angka2
+print(total)
---
+++ variable_studykasus.py
@@ -0,0 +1,13 @@
+# Variable
+# NIM : 1124102195 
+# NAMA : Scendy Aprianda Islamy
+
+#study kasus : buatkan operasi perkalian dengan menggunakan variable
+
+angka1 = 5
+angka2 = 10
+angka3 = 2
+
+total = angka1 * angka2 * angka3
+
+print(total)
