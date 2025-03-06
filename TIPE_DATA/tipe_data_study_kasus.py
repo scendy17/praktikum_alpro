981b1d9d7051bd38bdbb0527edfca3e3e945971e
kenn <scendy17@gmail.com>
2025-03-06 14:43:32 +0700

    Add files via upload

---
+++ TIPE_DATA/tipe_data_study_kasus.py
@@ -0,0 +1,13 @@
+# Variable
+# NIM : 1124102195 
+# NAMA : Scendy Aprianda Islamy
+
+# study kasus : buatkan program sederhana untuk menyimpan data mahasiswa. Data yang akan disimpan meliputi NIM, NAMA, dan JURUSAN.
+
+# sata mahasiswa
+mahasiswa = {'Nama':'Scendy Aprianda Islamy', 'NIM':'1124102195', 'Jurusan':'Teknik Informatika'}
+
+print('Data Mahasiswa :')
+print('Nama :', mahasiswa['Nama'])
+print('NIM :', mahasiswa['NIM'])
+print('Jurusan :', mahasiswa['Jurusan'])
---
+++ TIPE_DATA/tipedata.py
@@ -0,0 +1,29 @@
+# string : text
+data1 = 'Belajar Python'
+
+# integer : bilangan bulat
+data2 = 25
+
+# float : bilangan desimal
+data3 = 7.5
+
+# list : multiple data
+data4 = ['a','b', 'c']
+
+# tuple : multiple data
+data5 = ('a', 'b', 'c')
+
+# set : multiple data
+data6 = {'a', 'b', 'c'}
+
+# dictionary : multiple data
+data7 = {'nama': 'danu', 'umur': '21'}
+
+#tampilkan tipe data
+print('data1 tipe datanya :', type(data1))
+print('data2 tipe datanya :', type(data2))
+print('data3 tipe datanya :', type(data3))
+print('data4 tipe datanya :', type(data4))
+print('data5 tipe datanya :', type(data5))
+print('data6 tipe datanya :', type(data6))
+print('data7 tipe datanya :', type(data7))
