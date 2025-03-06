930e2117b01d36602e2d9b9ad2584a0c1a132720
kenn <scendy17@gmail.com>
2025-03-06 15:15:50 +0700

    Update input_study_kasus.py

--- INPUT_DATA/input_study_kasus.py
+++ INPUT_DATA/input_study_kasus.py
@@ -4,12 +4,15 @@
 
 #study kasus = buatkan inputan data mahasiswa dan menyimpannya didalam dictionary.
 
+#dictionary untuk menyimpan data
 mahasiswa = {}
 
+#inputan data dari mahasiswa
 mahasiswa['Nama'] = input('Masukkan Nama : ')
 mahasiswa['NIM'] = int(input('Masukkan NIM : '))
 mahasiswa['Jurusan'] = input('Masukkan Jurusan : ')
 
+#menampilkan data
 print('Terimakasih sudah memasukkan data')
 print('---------------------------------')
 print('Nama :', mahasiswa['Nama'])
