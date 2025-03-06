a3e939643f6d8b3f1b8faf902cf25b6521386af7
kenn <scendy17@gmail.com>
2025-03-06 14:57:25 +0700

    Add files via upload

---
+++ TIPE_DATA/INPUT_DATA/input_study_kasus.py
@@ -0,0 +1,18 @@
+# Variable
+# NIM : 1124102195 
+# NAMA : Scendy Aprianda Islamy
+
+#study kasus = buatkan inputan data mahasiswa dan menyimpannya didalam dictionary.
+
+mahasiswa = {}
+
+mahasiswa['Nama'] = input('Masukkan Nama : ')
+mahasiswa['NIM'] = int(input('Masukkan NIM : '))
+mahasiswa['Jurusan'] = input('Masukkan Jurusan : ')
+
+print('Terimakasih sudah memasukkan data')
+print('---------------------------------')
+print('Nama :', mahasiswa['Nama'])
+print('NIM :', mahasiswa['NIM'])
+print('Jurusan :', mahasiswa['Jurusan'])
+print('---------------------------------')
