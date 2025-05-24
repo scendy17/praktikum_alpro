ee2066d11fb2f7bdb5d0fecd211c90406694669d
kenn <scendy17@gmail.com>
2025-05-24 17:43:46 +0700

    Create set.py

---
+++ Pertemuan8/set.py
@@ -0,0 +1,11 @@
+#daftar belanja
+belanja = {'beras', 'gula', 'telur', 'minyak'}
+print("Daftar belanja awal:", belanja)
+
+#menambahkan barang
+belanja.add('susu')
+print("\nSetelah menambahkan susu:", belanja)
+
+#menghapus barang yang sudah ada
+belanja.remove('gula')
+print("\nSetelah menghapus gula:", belanja)
